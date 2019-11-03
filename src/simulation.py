10efb39b7622fad2023244dec9a63c637f793ee8
Telmo "Trooper <telmo.trooper@gmail.com>
2019-11-03 17:25:04 -0300

    Wrap initial text in Simulation

--- src/simulation.py
+++ src/simulation.py
@@ -1,13 +1,19 @@
 from component_list import ComponentList
+from stdout_redirector import stdout_redirector
+from io import BytesIO
 import libgenesys
 
 class Simulation:
   def __init__(self, handler):
     self.handler = handler
     self.cl = ComponentList()
-    self.simulator = libgenesys.Simulator()
-    libgenesys.PyGenesysApplication().insertFakePluginsByHand(self.simulator)
-    # A "self.model" object also has to be defined, but we'll leave that to the child
+    f = BytesIO()
+
+    with stdout_redirector(f):
+      self.simulator = libgenesys.Simulator()
+      libgenesys.PyGenesysApplication().insertFakePluginsByHand(self.simulator)
+      # A "self.model" object also has to be defined, but we'll leave that to the child
+    self.handler.print_to_log(f.getvalue().decode('utf-8'))
 
   def prepareSimulation(self):
     pass
